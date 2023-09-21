94b8dbbcbb9896e1b6ca62ec4b70cf0a4e37f636
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2023-09-21 23:01:58 +0530

    Update Triangle.c

--- Triangle.c
+++ Triangle.c
@@ -2,10 +2,10 @@
 int main(){
     printf("Enter the Base length:");
     float b;
-    scanf("%f",&b);
+    scanf("%f",&b);//input1
     printf("\nEnter the Height length:");
     float h;
-    scanf("%f",&h);
+    scanf("%f",&h);//input2
     printf("\nArea of the triangle: %f",0.5*b*h);
     printf("\nCircumference of the triangle: %f",3*b);
     return 0;
